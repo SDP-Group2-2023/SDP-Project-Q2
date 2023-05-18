3e70cfcb381ad7123b1fb58c3bafafc8fbd9a460
Edoardo Colella s257113 <s257113@studenti.polito.it>
2023-05-18 09:31:46 +0200

    tentativo di lettura di un file

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,37 @@
 #include <iostream>
+#include <fstream>
+#include <string>
 
 using namespace std;
 
 int main(int argc, char* argv[]) {
-  
-  return 0;
+
+  if(argc != 2) {
+    cout << "error: invalid arguments" << endl;
+    return 1;
+  }
+
+  ifstream file(argv[1]);
+  if(!file.is_open()) {
+    cout << "error: file not found" << endl;
+    return 1;
+  }
+
+  // leggo tutti i vertici
+  int N;
+  file >> N;
+  for (int i = 0; i<N; i++) {
+    int nodeCode, nodeWeight;
+    file >> nodeCode >> nodeWeight;
+    cout << nodeCode << " " << nodeWeight << endl;
+  }
+
+  // leggo tutti gli archi
+  int nodeCodeA, nodeCodeB, edgeWeight;
+  while(file >> nodeCodeA >> nodeCodeB >> edgeWeight) {
+    cout << nodeCodeA << " " << nodeCodeB << " " << edgeWeight << endl;
+  }
+
+    return 0;
 }
 
