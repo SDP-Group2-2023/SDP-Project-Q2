e0afa4fda5368bb84718f9e03e896178ecc68681
Edoardo Colella <edoardocolella.it@gmail.com>
2023-05-29 19:54:20 +0200

    script aggiornato per grafi sulle strade americane

--- src/tools/graphGenerator/graphGeneratorV2.py
+++ src/tools/graphGenerator/graphGeneratorV2.py
@@ -4,16 +4,32 @@ def generateGraph(inputFileName : str, outputFileName : str) -> None:
     f_in = open(inputFileName, "r")
     f_out = open(outputFileName, "x")
 
-    N = int(f_in.readline())
+    flag = False
+    while not flag:
+        line = f_in.readline().split()
+        if line[0] == "p":
+            flag = True
+            N = int(line[2])
+
+    print(f"Generating graph with {N} nodes...")
+
+    f_out.write(f"{N}\n")
+    for i in range(N):
+        f_out.write(f"{i+1} {random.randint(1,100)}\n")
+    N = int(int(line[3])/2)
+
+    print(f"Generating graph with {N} edges...")
+
     f_out.write(f"{N}\n")
     for _ in range(N):
-        val = f_in.readline()
-        f_out.write(f"{val.strip()} {random.randint(1,100)}\n")
+        line = f_in.readline().split()
+        f_in.readline()
+        f_out.write(f"{line[1]} {line[2]} {line[3]}\n")
     f_in.close()
     f_out.close()
 
 if __name__ == "__main__":
     if len(sys.argv[1:]) != 2:
-        print("Usage: python3 graphGeneratorV2.py <input_file> <output_file>")
+        print("Usage: python3 (or python on windows) graphGeneratorV2.py <input_file> <output_file>")
     else:
         generateGraph(sys.argv[1], sys.argv[2])
