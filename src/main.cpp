b29511f6fdb10f8c304a9900d75c9641992e7c4c
Edoardo Colella s257113 <s257113@studenti.polito.it>
2023-06-05 18:44:52 +0200

    menu with main functionalities

--- src/main.cpp
+++ src/main.cpp
@@ -4,34 +4,73 @@
 
 using namespace std;
 
+void loadGraph();
+void unloadGraph();
+void calculateSequential();
+void calculateParallel();
+void compareSolutions();
+
 int main(int argc, char* argv[]) {
+int choice;
+  bool valid = true;
 
-  if(argc != 2) {
-    cout << "error: invalid arguments" << endl;
-    return 1;
+  do{
+    cout << "1 - Load a graph from file" << endl;
+    cout << "2 - Unload the graph" << endl;
+    cout << "3 - Calculate a partition in a sequential way" << endl;
+    cout << "4 - Calculate a partition in a parallel way" << endl;
+    cout << "5 - Compare the sequential and parallel partition" << endl;
+    cout << "6 - Exit" << "\n" << endl;
+      cin >> choice;
+      if(cin.bad() || cin.fail()) {
+        cout << "Invalid choice" << endl;
+        cin.clear(); cin.ignore(10000, '\n');
+        continue;
+        }
+     switch (choice)
+  {
+  case 1:
+    loadGraph();
+    break;
+  case 2:
+    unloadGraph();
+    break;
+  case 3:
+    calculateSequential();
+    break;
+  case 4:
+    calculateParallel();
+    break;
+  case 5:
+    compareSolutions();
+    break;
+  case 6:
+    cout << "Good bye" << endl;
+    break;
+  default:
+    cout << "Invalid choice" << endl;
+    break;
   }
-
-  ifstream file(argv[1]);
-  if(!file.is_open()) {
-    cout << "error: file not found" << endl;
-    return 1;
-  }
-
-  // leggo tutti i vertici
-  int N;
-  file >> N;
-  for (int i = 0; i<N; i++) {
-    int nodeCode, nodeWeight;
-    file >> nodeCode >> nodeWeight;
-    cout << nodeCode << " " << nodeWeight << endl;
-  }
-
-  // leggo tutti gli archi
-  int nodeCodeA, nodeCodeB, edgeWeight;
-  while(file >> nodeCodeA >> nodeCodeB >> edgeWeight) {
-    cout << nodeCodeA << " " << nodeCodeB << " " << edgeWeight << endl;
-  }
-
-    return 0;
+  } while (choice != 6);
+      return 0;
 }
 
+void loadGraph() {
+  cout << "Loading graph" << endl;
+}
+
+void unloadGraph() {
+  cout << "Unloading graph" << endl;
+}
+
+void calculateSequential() {
+  cout << "Calculating sequential" << endl;
+}
+
+void calculateParallel() {
+  cout << "Calculating parallel" << endl;
+}
+
+void compareSolutions() {
+  cout << "Comparing solutions" << endl;
+}
