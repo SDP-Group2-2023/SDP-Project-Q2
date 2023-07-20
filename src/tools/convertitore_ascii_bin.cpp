e1642e2d73b7193234d556e67b08503798bb437a
Edoardo Colella s257113 <s257113@studenti.polito.it>
2023-07-20 11:17:48 +0200

    feat: step description in convertion fix: input and output close

--- src/tools/convertitore_ascii_bin.cpp
+++ src/tools/convertitore_ascii_bin.cpp
@@ -9,7 +9,18 @@ int main(int argc, char *argv[]){
     }
 
     ifstream input(argv[1]);
+    if(!input.is_open()){
+        cout << "Error opening file " << argv[1] << endl;
+        return 1;
+    }
+
     ofstream output(argv[2], ios::binary );
+    if(!output.is_open()){
+        cout << "Error opening file " << argv[2] << endl;
+        return 1;
+    }
+
+    cout << "Reading from " << argv[1] << " and writing to " << argv[2] << endl;
 
     char line[1024];
     input.getline(line, 1024);input.getline(line, 1024);
@@ -18,17 +29,19 @@ int main(int argc, char *argv[]){
     input >> a >> a >> num_nodes >> num_edges >> endline;
     output.write((char*)&num_nodes, sizeof(int));
     output.write((char*)&num_edges, sizeof(int));
-    //cout << num_nodes << " " << num_edges << endl;
+    cout << num_nodes << " " << num_edges << endl;
     input.getline(line, 1024);
     input.getline(line, 1024);
 
     int partenza, destinazione, distanza;
+    cout << "Writing nodes..." << endl;
     for(int i = 1; i<=num_nodes; i++){
         output.write((char*)&i, sizeof(int));
-        int random = rand()%1000;
+        int random = rand()%1000+1;
         output.write((char*)&random, sizeof(int));
     }
 
+    cout << "Writing edges..." << endl;
     for(int i = 0; i<num_edges; i++){
         input >> a >> partenza >> destinazione >> distanza;
         //cout << partenza << " " << destinazione << " " << distanza << endl;
@@ -37,5 +50,8 @@ int main(int argc, char *argv[]){
         output.write((char*)&distanza, sizeof(int));
     }
 
+    cout << "Done!" << endl;
+    input.close();
+    output.close();
     return 0;
 }
