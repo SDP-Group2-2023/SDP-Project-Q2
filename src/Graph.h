d705a498a6cb97f6c2db248fb9505700eac8711c
lidlessey3 <leonardo.pavarino@gmail.com>
2023-07-28 00:01:42 +0200

    added the loader from file using multiple threads to read the file

--- src/Graph.h
+++ src/Graph.h
@@ -3,6 +3,7 @@
 
 #include <vector>
 #include <map>
+#include <string>
 
 class Graph{
 private:
@@ -10,8 +11,13 @@ private:
     int num_edges;
     std::map<int, int> nodes_weights;
     std::vector<std::vector<int>> adj_matrix;
+
+    void read_nodes(std::string path, unsigned long offset, int to_read);
+    void read_edges(std::string path, unsigned long offset, int to_read);
+
 public:
     Graph(int num_nodes, int num_edges);
+    Graph(std::string path);
     void addEdge(int source, int dest, int weight);
     void addNodeWeight(int node, int weight);
     int getNumNodes() const;
