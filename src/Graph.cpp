9b9983c700c57dbb261252801f5664daa4908412
lidlessey3 <leonardo.pavarino@gmail.com>
2023-08-05 12:07:10 +0200

    feat: changed Graph from class to struct, moved constructor of edges and nodes from .h to .cpp

--- src/Graph.cpp
+++ src/Graph.cpp
@@ -2,17 +2,30 @@
 #include <iostream>
 
 using namespace std;
-Node* Graph::add_node(int id, int weight){
-    Node* n = new Node(id, weight);
+
+Node::Node(int id, int weight) {
+    this->id     = id;
+    this->weight = weight;
+    child        = nullptr;
+}
+
+Edge::Edge(int weight, Node *node1, Node *node2) {
+    this->weight = weight;
+    this->node1  = node1;
+    this->node2  = node2;
+    this->flag   = false;
+}
+
+Node *Graph::add_node(int id, int weight) {
+    Node *n = new Node(id, weight);
     nodes.push_back(n);
     this->V++;
     return n;
 }
 
-
 shared_ptr<Edge> Graph::add_edge(int source, int dest, int distance) {
-    Node* node1 = nodes[source];
-    Node* node2 = nodes[dest];
+    Node *node1        = nodes[source];
+    Node *node2        = nodes[dest];
     shared_ptr<Edge> e = make_shared<Edge>(distance, node1, node2);
     edges.push_back(e);
     node1->edges.push_back(e);
@@ -21,27 +34,25 @@ shared_ptr<Edge> Graph::add_edge(int source, int dest, int distance) {
     return e;
 }
 
-void Graph::print(){
+void Graph::print() {
     cout << "Graph with " << this->V << " nodes and " << this->E << " edges" << endl;
-    for(int i = 0; i<V; i++){
-        Node* n = nodes[i];
+    for (int i = 0; i < V; i++) {
+        Node *n = nodes[i];
         cout << "Node " << n->id << " with weight " << n->weight << endl;
-        for(auto & edge : n->edges) {
+        for (auto &edge : n->edges) {
             int source, dest;
             source = edge->node1->id;
-            dest = edge->node2->id;
-            if(source != n->id)
+            dest   = edge->node2->id;
+            if (source != n->id)
                 swap(source, dest);
-            cout << "\tEdge " << source << " -> "
-                 << dest << " with weight "
-                 << edge->weight << endl;
+            cout << "\tEdge " << source << " -> " << dest << " with weight " << edge->weight << endl;
         }
     }
 }
 
-Graph::~Graph(){
-    for(auto & node : nodes) {
-        for(auto & edge : node->edges) {
+Graph::~Graph() {
+    for (auto &node : nodes) {
+        for (auto &edge : node->edges) {
             edge = nullptr;
         }
         delete node;
@@ -49,9 +60,9 @@ Graph::~Graph(){
     }
 }
 
-void Graph::add_or_sum_edge(Node*n1, Node*n2, int distance) {
-    for(auto & edge : n1->edges) {
-        if(edge->node1 == n2 || edge->node2 == n2){
+void Graph::add_or_sum_edge(Node *n1, Node *n2, int distance) {
+    for (auto &edge : n1->edges) {
+        if (edge->node1 == n2 || edge->node2 == n2) {
             edge->weight += distance;
             return;
         }
--- src/Graph.h
+++ src/Graph.h
@@ -1,41 +1,30 @@
 #ifndef GRAPHPARTITIONING_GRAPH_H
 #define GRAPHPARTITIONING_GRAPH_H
 
-#include <vector>
+#include <algorithm>
 #include <iostream>
 #include <memory>
+#include <vector>
 
 using namespace std;
 
-struct Node{
-    Node(int id, int weight){
-        this->id = id;
-        this->weight = weight;
-        child = nullptr;
-    }
+struct Node {
+    Node(int id, int weight);
     int id;
     int weight;
     Node *child;
     vector<shared_ptr<struct Edge>> edges;
 };
 
-struct Edge{
-    Edge(int weight, Node* node1, Node* node2){
-        this->weight = weight;
-        this->node1 = node1;
-        this->node2 = node2;
-        this->flag = false;
-    }
+struct Edge {
+    Edge(int weight, Node *node1, Node *node2);
     int weight;
-    Node* node1;
-    Node* node2;
-    bool flag ;
+    Node *node1;
+    Node *node2;
+    bool flag;
 };
 
-
-
-class Graph {
-public:
+struct Graph {
     int V = 0;
     int E = 0;
     vector<Node *> nodes;
@@ -43,9 +32,8 @@ public:
     Node *add_node(int id, int weight);
     shared_ptr<Edge> add_edge(int source, int dest, int distance);
     void print();
-    void add_or_sum_edge(Node*n1, Node*n2, int distance);
+    void add_or_sum_edge(Node *n1, Node *n2, int distance);
     ~Graph();
- };
+};
 
-
-#endif //GRAPHPARTITIONING_GRAPH_H
+#endif    // GRAPHPARTITIONING_GRAPH_H
