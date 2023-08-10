ca1d254bfa2b4925b8eb00bc0a1d0dc242bf9528
lidlessey3 <leonardo.pavarino@gmail.com>
2023-08-10 21:58:30 +0200

    FIX: changed the < operator, so now it distinguishes by type.

--- src/Change.cpp
+++ src/Change.cpp
@@ -4,6 +4,7 @@ Change::Change(int new_partition, Node *node, int gain) : new_partition(new_part
 
 Change::Change() : new_partition(-1), node(nullptr), gain(-1) {}
 
-bool Change::operator< (const Change &other) const {
-    return (this->gain > other.gain) || ((this->gain == other.gain) && this->node > other.node);
+bool Change::operator<(const Change &other) const {
+    return (this->gain > other.gain) || (this->gain == other.gain && this->node > other.node) ||
+           (this->node == other.node && this->gain == other.gain && this->new_partition > other.new_partition);
 }
