85d0533b30aa355addd85f677300b1802288a85c
lidlessey3 <leonardo.pavarino@gmail.com>
2023-09-14 22:59:26 +0200

    began sequential initial partitioner

--- src/sequential_partitioning.cpp
+++ src/sequential_partitioning.cpp
@@ -1,42 +1,46 @@
 #include "Graph.h"
 #include "partitioning.h"
-#include <vector>
 #include <iostream>
+#include <vector>
 
 using namespace std;
-vector<int> uncoarsenGraph(Graph* g, vector<int>& partitions){
+vector<int> uncoarsenGraph(Graph *g, vector<int> &partitions) {
     vector<int> newPartitions(g->V());
 
-    for(auto&n : g->nodes)
+    for (auto &n : g->nodes)
         newPartitions[n->id] = partitions[n->child->id];
 
     return newPartitions;
 }
 
-void partitioning_s(Graph*g, int requestedPartitions){
+void initial_partitioning_s(Graph *g, vector<int> &partitions, int partition_num) {
+    for (int i = 0; i < g->V(); i++) {
+        partitions[i] = i % partition_num;
+    }
+}
+
+void partitioning_s(Graph *g, int requestedPartitions) {
     int actual_num_partitions = g->V();
 
-    vector<Graph*> allGraphs;
+    vector<Graph *> allGraphs;
     allGraphs.push_back(g);
 
     int iterations = 0;
-    while( actual_num_partitions > requestedPartitions * 15  && iterations++ < 50){
+    while (actual_num_partitions > requestedPartitions * 15 && iterations++ < 50) {
         cout << "Iteration " << iterations << endl;
-        Graph* coarsedGraph = coarseGraph_s(allGraphs.back());
-        //coarsedGraph->print();
+        Graph *coarsedGraph = coarseGraph_s(allGraphs.back());
+        // coarsedGraph->print();
         actual_num_partitions = coarsedGraph->V();
         cout << "Actual number of partitions: " << actual_num_partitions << endl;
         allGraphs.push_back(coarsedGraph);
     }
 
-    Graph* coarsestGraph = allGraphs[allGraphs.size()-1];
+    Graph *coarsestGraph = allGraphs[allGraphs.size() - 1];
     vector<int> partitions(coarsestGraph->V());
-    for(int i = 0; i<coarsestGraph->V(); i++){
-        partitions[i] = i % requestedPartitions;
-    }
+    initial_partitioning_s(coarsestGraph, partitions, requestedPartitions);
 
     kernighanLin(coarsestGraph, requestedPartitions, partitions);
-    
+
     for (auto i = (int) allGraphs.size() - 2; i >= 0; i--) {
         partitions = uncoarsenGraph(allGraphs[i], partitions);
         cout << "Uncoarsening step " << i << endl;
@@ -44,12 +48,11 @@ void partitioning_s(Graph*g, int requestedPartitions){
         kernighanLin(allGraphs[i], requestedPartitions, partitions);
     }
 
-    for(int i = 0; i<partitions.size(); i++){
+    for (int i = 0; i < partitions.size(); i++) {
         cout << "Node " << i << " in partition " << partitions[i] << endl;
     }
 
-    for(int i = 1; i<allGraphs.size(); i++){
+    for (int i = 1; i < allGraphs.size(); i++) {
         delete allGraphs[i];
     }
 }
-
