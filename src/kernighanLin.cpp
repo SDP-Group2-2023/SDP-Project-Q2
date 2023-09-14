eb669a391e94bec76f85df1eec8440c5f27bd5e7
lidlessey3 <leonardo.pavarino@gmail.com>
2023-09-14 22:55:14 +0200

    FIX: optimized the creation of the possible changes, so that it is now both quicker and does not generate useless entries

--- src/kernighanLin.cpp
+++ src/kernighanLin.cpp
@@ -25,7 +25,7 @@ unsigned long long calculateCutSize(Graph *graph, vector<int> &partitions) {
             source = edge->node1->id;
             dest   = edge->node2->id;
             if (partitions[source] != partitions[dest])
-                cutsize += edge->weight >> 1;   // divided by 2
+                cutsize += edge->weight >> 1;    // divided by 2
         }
     }
     return cutsize;
@@ -55,7 +55,6 @@ void kernighanLin(Graph *graph, int num_partitions, vector<int> &partitions) {
     unsigned long long best_cut_size = cut_size;
     vector<int> best_partitions(partitions);
 
-
     /* Partition weight calculation has been anticipated outside the do-while in order to improve timing performance:
      * indeed "possible_changes" computation is proportional to V*P (number of nodes*number of partitions) and recomputing
      * partitions' weights is proportional to V --> the complexity of the for loop (on the possible_changes) was P*V^2 quadratic in V
@@ -83,10 +82,10 @@ void kernighanLin(Graph *graph, int num_partitions, vector<int> &partitions) {
         for (int i = 0; i < graph->V(); i++) {
             Node *current_node = graph->nodes[i];
 
-            for (int p = 0; p < num_partitions; p++) {    // assign node to all possible partitions other than his
-                if (p != partitions[current_node->id]) {
-                    possible_changes.emplace(p, current_node, gain( partitions, current_node, p));
-                    node_gain_mapping[current_node][p] = gain(partitions, current_node, p);
+            for (auto n : current_node->get_neighbors()) {    // assign node to all possible partitions other than his
+                if (partitions[n->id] != partitions[current_node->id]) {
+                    possible_changes.emplace(partitions[n->id], current_node, gain(partitions, current_node, partitions[n->id]));
+                    node_gain_mapping[current_node][partitions[n->id]] = gain(partitions, current_node, partitions[n->id]);
                 }
             }
         }
@@ -147,39 +146,27 @@ void kernighanLin(Graph *graph, int num_partitions, vector<int> &partitions) {
             // managing stopping criterion
             sum_of_gains += best_change.gain;
 
-            // update necessary gain values in the set (neighbours of the seòected node)
-            // no longer useful since every node is moved only once in one iteration of inner loop
-            for (int i = 0; i < num_partitions; i++) {
-
-                // removing selected change from "possible_changes" set
-                if (i != partitions[best_change.node->id]) {
-                    Change new_change;
-                    new_change.node          = best_change.node;
-                    new_change.new_partition = i;
-                    if (i != old_partition) {
-                        new_change.gain = node_gain_mapping[new_change.node][i];
-                        possible_changes.erase(new_change);
-                    }
-                }
-            }
-
             // update gains of all neighbouring nodes of new_change.node
             for (auto n : best_change.node->get_neighbors()) {
-                for (int i = 0; i < num_partitions; i++) {
-                    // removing selected change from possible_changes set (remember that to remove elements from a set all details of the element must be provided to erase function)
+                for (auto neighbour : n->get_neighbors()) {
+                    int i = partitions[neighbour->id];
+                    // removing selected change from possible_changes set (remember that to remove elements from a set all details of the element must be provided to erase
+                    // function)
                     if (i != partitions[n->id]) {
                         Change new_change;
                         new_change.node          = n;
                         new_change.new_partition = i;
 
-                        new_change.gain = node_gain_mapping[n][i];
-                        possible_changes.erase(new_change);
+                        if (node_gain_mapping.contains(n) && node_gain_mapping[n].contains(i)) {
+                            new_change.gain = node_gain_mapping[n][i];
+                            possible_changes.erase(new_change);
+                        }
 
                         // update gain of selected node to all other partitions and insert change in set
-                        new_change.gain = gain( partitions, n, i);
+                        new_change.gain = gain(partitions, n, i);
                         possible_changes.insert(new_change);
                         // update node gain mapping for further references
-                        node_gain_mapping[n][i] = gain( partitions, n, i);
+                        node_gain_mapping[n][i] = gain(partitions, n, i);
                     }
                 }
             }
@@ -196,13 +183,14 @@ void kernighanLin(Graph *graph, int num_partitions, vector<int> &partitions) {
             num_iteration++;
         }
 
-        partitions                = best_partitions;
-        cut_size                  = best_cut_size;
-        graph->partitions_size    = best_partitions_weights;
+        partitions             = best_partitions;
+        cut_size               = best_cut_size;
+        graph->partitions_size = best_partitions_weights;
 
-        //cout << "With " << tot_moves << " moves, we moved the cut size to " << cut_size << endl;
-        //cout << "last iterations: " << iteration << " max_iteration: " << max_iteration << " avg_iteration " << avg_iteration / num_iteration << endl;
-        //cout << "Total time spent creating choices: " << choices_loop.getDuration().count() << " - total time spent making choices: " << choosing_loop.getDuration().count() << endl;
+        // cout << "With " << tot_moves << " moves, we moved the cut size to " << cut_size << endl;
+        // cout << "last iterations: " << iteration << " max_iteration: " << max_iteration << " avg_iteration " << avg_iteration / num_iteration << endl;
+        // cout << "Total time spent creating choices: " << choices_loop.getDuration().count() << " - total time spent making choices: " << choosing_loop.getDuration().count() <<
+        // endl;
 
     } while (improved);
 }
