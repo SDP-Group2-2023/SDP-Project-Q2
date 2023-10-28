82988866d7875f981a14c7071c55339ab67fd574
Edoardo Colella s257113 <s257113@studenti.polito.it>
2023-10-28 23:07:58 +0200

    Minor fix4 in import

--- src/tools/read_bin.cpp
+++ src/tools/read_bin.cpp
@@ -4,18 +4,18 @@
 
 int main(int argc, char**argv){
     if(argc < 2){
-        cout << "Usage: " << argv[0] << " <input file>"  << endl;
+        std::cout << "Usage: " << argv[0] << " <input file>"  << std::endl;
         return 1;
     }
-    fstream input(argv[1], ios::binary | ios::in);
+    std::fstream input(argv[1], std::ios::binary | std::ios::in);
     if(!input.is_open()){
-        cout << "Error opening file " << argv[1] << endl;
+        std::cout << "Error opening file " << argv[1] << std::endl;
         return 1;
     }
 
-    ofstream output("temp.txt", ios::out);
+    std::ofstream output("temp.txt", std::ios::out);
     if(!output.is_open()){
-        cout << "Error opening file temp.txt" << endl;
+        std::cout << "Error opening file temp.txt" << std::endl;
         return 1;
     }
 
@@ -23,9 +23,9 @@ int main(int argc, char**argv){
     unsigned long num_edges;
     input.read((char*)&num_nodes, sizeof(unsigned int));
     input.read((char*)&num_edges, sizeof(unsigned long));
-    cout << "num_nodes " << num_nodes << endl;
-    cout << "num_edges " << num_edges << endl;
-    output << num_nodes << " " << num_edges << endl;
+    std::cout << "num_nodes " << num_nodes << std::endl;
+    std::cout << "num_edges " << num_edges << std::endl;
+    output << num_nodes << " " << num_edges << std::endl;
 
     return 0;
 
@@ -36,11 +36,11 @@ int main(int argc, char**argv){
         input.read((char*)&node, sizeof(unsigned int));
         input.read((char*)&node_weight, sizeof(unsigned int));
 
-        cout << node << " " << node_weight << endl;
+        std::cout << node << " " << node_weight << std::endl;
         //output << node << " " << node_weight << endl;
 
         if(node > num_nodes-1) {
-            cout << "Error: node " << node << " is greater than num_nodes " << num_nodes << endl;
+            std::cout << "Error: node " << node << " is greater than num_nodes " << num_nodes << std::endl;
             return 1;
         }
 
@@ -56,15 +56,15 @@ int main(int argc, char**argv){
         input.read((char*)&distance, sizeof(unsigned int));
 
         if(source > num_nodes-1) {
-            cout << "Error: source " << source << " is greater than num_nodes " << num_nodes << endl;
+            std::cout << "Error: source " << source << " is greater than num_nodes " << num_nodes << std::endl;
             return 1;
         }
         if(dest > num_nodes-1) {
-            cout << "Error: dest " << dest << " is greater than num_nodes " << num_nodes << endl;
+            std::cout << "Error: dest " << dest << " is greater than num_nodes " << num_nodes << std::endl;
             return 1;
         }
 
-        cout << source << " " << dest << " " << distance << endl;
+        std::cout << source << " " << dest << " " << distance << std::endl;
         //output << source << " " << dest << " " << distance << endl;
     }
 }
